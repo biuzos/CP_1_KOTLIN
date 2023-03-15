a5e8bbd8a24d7f61b4a3dbaf181032b70a537753
biuzos <bielhenko@gmail.com>
2023-03-15 14:41:03 -0300

    primeiro commit

--- app/src/main/java/fiap/com/br/MainActivity.kt
+++ app/src/main/java/fiap/com/br/MainActivity.kt
@@ -1,5 +1,6 @@
 package fiap.com.br
-
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButtonAndroid = findViewById<Button>(R.id.bttn_android)
+        val myButtonIOS= findViewById<Button>(R.id.bttn_ios)
+        val myTextViewResultado = findViewById<TextView>(R.id.resultado)
+
+        myButtonAndroid.setOnClickListener {
+
+            myTextViewResultado.text = "Você prefere Android!"
+        }
+        myButtonIOS.setOnClickListener {
+            myTextViewResultado.text = "Você prefere IOS!"
+        }
+
     }
+
+
+
+
+
+
 }
